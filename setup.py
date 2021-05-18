59aba34d634f80ee89272a6d94bb6ad918693bb8
Ravid Levy <ravidlevy99@gmail.com>
2021-05-18 21:45:51 +0300

    Added setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='EDClientServer',
+    version='0.0.1',
+    description='Our package for the communication module between Event Detector and Automated Microscope',
+    url='https://github.com/ravidlevy99/EDClientServer',
+    author='Matanel Rozenberg, Ravid Levy, Viktor Koukouliev',
+    packages=['communication'],
+    zip_safe=False
+)
