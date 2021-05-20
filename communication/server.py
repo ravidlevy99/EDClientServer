c1fbb4829a541c4ef766e64a4185e85a01d08e06
matanelrb <matanelrb@gmail.com>
2021-05-20 22:07:24 +0300

    added producer consumer to server

--- communication/server.py
+++ communication/server.py
@@ -1,12 +1,15 @@
 import communication.protocol as protocol
 import socket
 from abc import ABC, abstractmethod
+import queue
+from threading import Thread
 
 
 class Server(ABC):
     def __init__(self, port, chosen_protocol):
         self.conn = setup_connection('localhost', port)
         self.protocol = chosen_protocol
+        self.queue = queue.Queue()
         if self.get_syn_request(chosen_protocol):
             self.start_server_loop()
 
@@ -29,17 +32,30 @@ class Server(ABC):
     def answer(self, data):
         self.conn.sendall(bytes(data, encoding="utf-8"))
 
+    def ed_handle_request_loop(self):
+        while True:
+            data = self.queue.get()
+
+            ed_data = self.handle_request(data)
+            response = protocol.create_ed_response(self.protocol, ed_data)
+            self.answer(response)
+
+            self.queue.task_done()
+
     def start_server_loop(self):
+
+        ed_handling_thread = Thread(target=self.ed_handle_request_loop, daemon=True)
+        ed_handling_thread.start()
+
         while True:
             request = self.get_request()
             try:
                 data = protocol.parse_request(request, [self.protocol, protocol.TERMINATE])
                 if data == protocol.TERMINATE:
+                    self.on_termination()
                     break
+                self.queue.put(data)
 
-                ed_data = self.handle_request(data)
-                response = protocol.create_ed_response(self.protocol, ed_data)
-                self.answer(response)
             except Exception as e:
                 error = protocol.create_error_response(str(e))
                 self.answer(error)
@@ -47,6 +63,9 @@ class Server(ABC):
 
     @abstractmethod
     def handle_request(self, data):
+        pass
+
+    def on_termination(self):
         pass
 
 
