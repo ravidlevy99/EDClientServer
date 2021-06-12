f3f1fc9df9bc4dac78657dbd6dd4e30404f5f5e8
matanelrb <matanelrb@gmail.com>
2021-06-12 16:49:25 +0300

    version change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='EDClientServer',
-    version='0.0.6',
+    version='0.0.7',
     description='Our package for the communication module between Event Detector and Automated Microscope',
     url='git@github.com:ravidlevy99/EDClientServer.git',
     author='Matanel Rozenberg, Ravid Levy, Viktor Koukouliev',
